e6d065dd1f9ffce94dee3ef53ff65f18db4d52c9
Jawann <jawann@gmail.com>
2017-10-26 03:54:04 +0000

    automatically backed up by learn

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -88,7 +88,7 @@ describe './lib/tic_tac_toe.rb' do
 
   describe '#position_taken?' do
     it 'returns true/false based on position in board' do
-      board = ["X", " ", " ", " ", " ", " ", " ", "", "O"]
+      board = ["X", " ", " ", " ", " ", " ", " ",, "O"]
 
       position = 0
       expect(position_taken?(board, position)).to be(true)
