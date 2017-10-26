eea35ab80b68bc9b39d240d78aede2fef9104c32
Jawann <jawann@gmail.com>
2017-10-26 11:29:23 +0000

    automatically backed up by learn

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -82,13 +82,13 @@ describe './lib/tic_tac_toe.rb' do
       move(board, 0, "O")
       move(board, 8, "X")
 
-      expect(board).to eq(["O", " ", " ", " ", "", " ", " ", " ", "X"])
+      expect(board).to eq(["O", " ", " ", " ", " ", " ", " ", " ", "X"])
     end
   end
 
   describe '#position_taken?' do
     it 'returns true/false based on position in board' do
-      board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
+      board = ["X", " ", " ", " ", "", " ", " ", " ", "O"]
 
       position = 0
       expect(position_taken?(board, position)).to be(true)
@@ -101,6 +101,9 @@ describe './lib/tic_tac_toe.rb' do
 
       position = 7
       expect(position_taken?(board, position)).to be(false)
+
+      position = 4
+      expect(position_taken?(board, position)).to be_falsey
     end
   end
 
